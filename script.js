9dce8a4a769d34617411228a20710181ef7e17db
ethann <ethann@vt.edu>
2024-03-06 23:27:07 -0500

    mobile

--- script.js
+++ script.js
@@ -1,48 +1,87 @@
-const emailValidator = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/";
+const emailValidator = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
 
 function subscribeClicked() {
-
-    console.log("Hello");
 
     const input = document.getElementById("emailInput");
 
     const email = input.value; 
+    
+    console.log(isInvalidEmail(email));
 
     if (isInvalidEmail(email)) {
         emailError();
+        return;
     }
-    else {
-        removeEmailError();
-    }
+
+    removeEmailError();
+
+    const signUpContent = document.getElementById("signUpContent");
+    const successPage = document.getElementById("success");
+    const successP = document.getElementById("successMessage");
+
+    signUpContent.classList.add("hidden");
+    signUpContent.classList.remove("content");
+
+    successPage.classList.remove("hidden");
+    successPage.classList.add("successContainer");
+
+    successP.innerHTML = "A confirmation email has been sent to <b>" +  email + "</b> Please open it and click the button inside to confirm your subscription."
+}
+
+function dismissClicked() {
+
+    const signUpPage = document.getElementById("signUpContent");
+    const successPage = document.getElementById("success");
+
+    signUpPage.classList.remove("hidden");
+    signUpPage.classList.add("content");
+
+    successPage.classList.remove("successContainer");
+    successPage.classList.add("hidden");
 }
 
 function isInvalidEmail(email) {
 
-    return email.match(emailValidator);
+    return !emailValidator.test(email);
 }
 
 function emailError() {
 
     const emailError = document.getElementById("emailErrorMessage");
+    const input = document.getElementById("emailInput");
 
     if (emailError.classList.contains("errorHidden")) {
         emailError.classList.remove("errorHidden");
         emailError.classList.add("errorShown");
+    }
+
+    if (!input.classList.contains("inputError")) {
+        input.classList.add("inputError");
     }
 }
 
 function removeEmailError() {
 
     const emailError = document.getElementById("emailErrorMessage");
+    const input = document.getElementById("emailInput");
+
 
     if (emailError.classList.contains("errorShown")) {
         emailError.classList.remove("errorShown");
         emailError.classList.add("errorHidden");
     }
+
+    if (input.classList.contains("inputError")) {
+        input.classList.remove("inputError");
+    }
 }
 
-const button = document.getElementById("subscribeButton");
+const subButton = document.getElementById("subscribeButton");
+const disButton = document.getElementById("disButton");
 
-button.addEventListener("click", subscribeClicked);
+const input = document.getElementById("emailInput");
 
-console.log("Hi");
+subButton.addEventListener("click", subscribeClicked);
+disButton.addEventListener("click", dismissClicked);
+
+input.addEventListener("focus", () => removeEmailError());
